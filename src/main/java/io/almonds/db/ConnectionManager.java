7b111dafcf52706261ec3127dc0ea99391dde0dd
jyun <Poex0716@gmail.com>
2024-08-29 07:30:35 +0800

    ;DB_CLOSE_DELAY=-1 for table not found issue

--- src/main/java/io/almonds/db/ConnectionManager.java
+++ src/main/java/io/almonds/db/ConnectionManager.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectionManager {
 
   public static Connection getConnection() throws SQLException {
-    String url = "jdbc:h2:mem:mydb";
+    String url = "jdbc:h2:mem:mydb;DB_CLOSE_DELAY=-1";
     String id = System.getenv("DB_ID");
     String password = System.getenv("DB_SECRET");
     return DriverManager.getConnection(url, id, password);
