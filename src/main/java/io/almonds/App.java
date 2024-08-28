aa7e86858d341da3da6cb86e42e86f27f93e6e8f
jyun <Poex0716@gmail.com>
2024-08-28 19:59:05 +0800

    adding interactive cli

--- src/main/java/io/almonds/App.java
+++ src/main/java/io/almonds/App.java
@@ -1,23 +1,17 @@
 package io.almonds;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import io.almonds.db.ConnectionManager;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws SQLException {
-        try (Connection connection = ConnectionManager.getConnection()) {
-            String sql = "SELECT 1";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                    while (resultSet.next()) {
-                        System.out.println(resultSet.getString(1));
-                    }
-                }
+        try (Scanner scanner = new Scanner(System.in)) {
+            String input;
+            System.out.println("Please specify an action: [create(c)/read(r)/update(u)/delete(d)]");
+            while (!"exit".equals(input = scanner.nextLine())) {
+                System.out.println(input);
             }
         }
     }
+
 }
