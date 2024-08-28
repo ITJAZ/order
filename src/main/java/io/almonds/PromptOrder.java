b00d44343466d1f7331c485b99b591456a8378f9
jyun <Poex0716@gmail.com>
2024-08-28 20:48:02 +0800

    delete function implementation

--- src/main/java/io/almonds/PromptOrder.java
+++ src/main/java/io/almonds/PromptOrder.java
@@ -89,10 +89,73 @@ public class PromptOrder {
   }
 
   public void update() {
+    System.out.println("Updating Mode... ");
+    Order order = null;
 
+    while (order == null) {
+      System.out.println("Please enter the [Order No] of the order you want to update: ");
+      String orderNoString = scanner.nextLine();
+      try {
+        int orderNo = Integer.parseInt(orderNoString);
+        order = orderService.getOrderByNo(orderNo);
+      } catch (Exception e) {
+        System.err.printf("Invalid order no input: %s, only accept number... \n", orderNoString);
+        System.out.println("Please enter the order no: ");
+        orderNoString = scanner.nextLine();
+      }
+    }
+
+    System.out.println("Please enter your item name: ");
+    String item = scanner.nextLine();
+    System.out.println("Please enter your sugar level: ");
+    String sugarLevel = scanner.nextLine();
+    System.out.println("Please enter your ice level: ");
+    String iceLevel = scanner.nextLine();
+    System.out.println("Please enter the price: ");
+    String priceString = scanner.nextLine();
+
+    BigDecimal price;
+    while (true) {
+      try {
+        price = new BigDecimal(priceString);
+        break;
+      } catch (Exception e) {
+        System.err.printf("Invalid price input: %s, only accept number... \n", priceString);
+        System.out.println("Please enter the price: ");
+        priceString = scanner.nextLine();
+        continue;
+      }
+    }
+
+    order.setItem(item);
+    order.setIceLevel(iceLevel);
+    order.setSugarLevel(sugarLevel);
+    order.setPrice(price);
+
+    orderService.updateOrder(order.getOrderNo(), order);
+
+    System.out.println("Order Updated.");
+    System.out.println(order);
+    System.out.println("Current Orders: ");
+    System.out.println(orderService.getOrders());
   }
 
   public void delete() {
-
+    System.out.println("Deleting Mode... ");
+    int orderNo;
+    while (true) {
+      System.out.println("Please enter the [Order No] of the order you want to delete: ");
+      String orderNoString = scanner.nextLine();
+      try {
+        orderNo = Integer.parseInt(orderNoString);
+        break;
+      } catch (Exception e) {
+        System.err.printf("Invalid order no input: %s, only accept number... \n", orderNoString);
+      }
+    }
+    orderService.deleteOrder(orderNo);
+    System.out.println("Order deleted: " + orderNo);
+    System.out.println("Current Orders: ");
+    System.out.println(orderService.getOrders());
   }
 }
