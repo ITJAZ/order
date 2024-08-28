a7d868c9f468dcb203572523af5cfc3cc7b6d048
jyun <Poex0716@gmail.com>
2024-08-28 20:32:34 +0800

    hint after action executed

--- src/main/java/io/almonds/App.java
+++ src/main/java/io/almonds/App.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class App {
-    private static final String COMMAND_HINT = "Please specify an action: [create(c)/read(r)/update(u)/delete(d)/export(e)]";
+    private static final String COMMAND_HINT = "Please specify an action: [create(c)/read(r)/update(u)/delete(d)/export(e)/exit]";
 
     public static void main(String[] args) throws SQLException {
         try (Scanner scanner = new Scanner(System.in)) {
@@ -19,18 +19,22 @@ public class App {
                     case "create":
                     case "c":
                         promptOrder.create();
+                        System.out.println(COMMAND_HINT);
                         break;
                     case "read":
                     case "r":
                         promptOrder.read();
+                        System.out.println(COMMAND_HINT);
                         break;
                     case "update":
                     case "u":
                         promptOrder.update();
+                        System.out.println(COMMAND_HINT);
                         break;
                     case "delete":
                     case "d":
                         promptOrder.delete();
+                        System.out.println(COMMAND_HINT);
                         break;
                     default:
                         System.out.printf("Unknown command: %s \n", input);
