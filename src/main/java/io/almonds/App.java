5f477694a98cad9ac41ab3e3f7eedec67620956c
jyun <Poex0716@gmail.com>
2024-08-28 20:19:56 +0800

    adding prompt create

--- src/main/java/io/almonds/App.java
+++ src/main/java/io/almonds/App.java
@@ -6,10 +6,31 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws SQLException {
         try (Scanner scanner = new Scanner(System.in)) {
+            PromptOrder promptOrder = new PromptOrder(scanner);
             String input;
             System.out.println("Please specify an action: [create(c)/read(r)/update(u)/delete(d)]");
             while (!"exit".equals(input = scanner.nextLine())) {
-                System.out.println(input);
+                if (input == null)
+                    continue;
+
+                switch (input.toLowerCase()) {
+                    case "create":
+                    case "c":
+                        promptOrder.create();
+                        break;
+                    case "read":
+                    case "r":
+                        promptOrder.read();
+                        break;
+                    case "update":
+                    case "u":
+                        promptOrder.update();
+                        break;
+                    case "delete":
+                    case "d":
+                        promptOrder.delete();
+                        break;
+                }
             }
         }
     }
---
+++ src/main/java/io/almonds/PromptOrder.java
@@ -0,0 +1,65 @@
+package io.almonds;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+import io.almonds.entity.Order;
+import io.almonds.service.OrderService;
+
+public class PromptOrder {
+
+  private Scanner scanner;
+
+  private OrderService orderService;
+
+  public PromptOrder(Scanner scanner) {
+    this.scanner = scanner;
+    this.orderService = new OrderService();
+  }
+
+  public void create() {
+    System.out.println("Please enter your item name: ");
+    String item = scanner.nextLine();
+    System.out.println("Please enter your sugar level: ");
+    String sugarLevel = scanner.nextLine();
+    System.out.println("Please enter your ice level: ");
+    String iceLevel = scanner.nextLine();
+    System.out.println("Please enter the price: ");
+    String priceString = scanner.nextLine();
+    int tried = 0;
+    BigDecimal price = null;
+    while (tried++ < 3) {
+      try {
+        price = new BigDecimal(priceString);
+      } catch (Exception e) {
+        System.err.printf("Invalid price input: %s, only accept number... \n", priceString);
+        continue;
+      }
+    }
+
+    if (price == null) {
+      System.err.println("Too many invalid tried, aborting...");
+      return;
+    }
+
+    Order order = orderService.createAnOrder(item, sugarLevel, iceLevel, price);
+
+    System.out.println("Order Created.");
+    System.out.println(order);
+    System.out.println("Current Orders: ");
+    System.out.println(orderService.getOrders());
+    return;
+  }
+
+  public void read() {
+
+  }
+
+  public void update() {
+
+  }
+
+  public void delete() {
+
+  }
+}
